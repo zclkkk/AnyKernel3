e48bfb8457b40766803c97826344aa93c96e5457
zclkkk <zclkkk@outlook.com>
2026-02-26 00:41:00 +0800

    Stop flashing vendor_boot

--- anykernel.sh
+++ anykernel.sh
@@ -46,18 +46,3 @@ fi;
 
 write_boot;
 ## end install
-
-## vendor_boot shell variables
-block=/dev/block/bootdevice/by-name/vendor_boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-reset_ak;
-
-# vendor_boot install
-dump_boot;
-
-write_boot;
-## end vendor_boot install
